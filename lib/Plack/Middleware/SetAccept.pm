6f5a1f45cd473293ad75275bc90250d9064ae9a5
Rob Hoelz <rob@hoelz.ro>
2011-06-02 19:57:52 -0500

    initial commit

---
+++ lib/Plack/Middleware/SetAccept.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Plack::Middleware::SetAccept;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A brief description of your module goes here
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
